2d157212d901f9ac4de7c30467c1d12ac95f015f
yuwen <yuwen4113@gmail.com>
2023-06-28 17:04:54 +0800

    feat: add Keys

---
+++ CoinProject/CoinProject/ApiKeys.swift
@@ -0,0 +1,14 @@
+//
+//  ApiKeys.swift
+//  CoinProject
+//
+//  Created by 0000 on 2023/6/28.
+//
+
+import Foundation
+
+struct ApiKeys {
+    let apiKey = "vbardmV8TY36EcoJ7Mf4bHIAJ3yFZUC4xqKIE9jUUaCVIyneAuB+7GiBByYq4NDTfyjx7hUbDgGWQl+Qu4Tqhw=="
+    let accessKey = "4aaf38fb954b671ad5ef96821fc3827d"
+    let accessPassphrase = "wjnqm87rvjp"
+}
